f162bf8b8acee4a97dd88b1eef05c4a9ce82111c
Gary <gear@thytruth.com>
2019-09-07 20:25:43 +0800

    update

--- api/bin/run.sh
+++ api/bin/run.sh
@@ -22,5 +22,7 @@
 #source venv/bin/activate
 export APP_HOME=/opt/api
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${APP_HOME}/bin:${APP_HOME}/sbin
-
-exec gunicorn -b :8021 --access-logfile - --error-logfile - run:app
+export caller="api"
+cd $API_HOME/bin
+gunicorn -w $THREADS -t 120 -b :8021 --access-logfile --error-logfile run:app
+#gunicorn -b :8021 --access-logfile - --error-logfile - run:app
