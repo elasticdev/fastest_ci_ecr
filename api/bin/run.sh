df4309545a1faf085e040732a1d00b6ae649f52c
Gary <gear@thytruth.com>
2019-09-07 20:28:24 +0800

    update

--- api/bin/run.sh
+++ api/bin/run.sh
@@ -25,6 +25,10 @@ export THREADS=2
 export APP_HOME=/opt/api
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${APP_HOME}/bin:${APP_HOME}/sbin
 export caller="api"
-cd $API_HOME/bin
+cd $APP_HOME/bin
+pwd
+pwd
+pwd
+pwd
 gunicorn -w $THREADS -t 120 -b :8021 --access-logfile - --error-logfile - run:app
 #gunicorn -b :8021 --access-logfile - --error-logfile - run:app
