f0e1e6f97119f9745e5bed4ef3fdc0ef9d732b30
Gary <gear@thytruth.com>
2019-11-05 12:49:14 -0800

    update

--- ci/install_trivy_ubuntu.sh
+++ ci/install_trivy_ubuntu.sh
@@ -1,7 +1,12 @@
-apt-get update
-CODE_NAME=`lsb_release -c|cut -d ":" -f 2`
-apt-get install apt-transport-https gnupg -y
+#apt-get update
+#CODE_NAME=`lsb_release -c|cut -d ":" -f 2`
+#apt-get install apt-transport-https gnupg -y
+#wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
+#echo deb https://aquasecurity.github.io/trivy-repo/deb $CODE_NAME main | tee -a /etc/apt/sources.list.d/trivy.list
+#apt-get update
+#apt-get install trivy -y
+apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $CODE_NAME main | tee -a /etc/apt/sources.list.d/trivy.list
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list.d/trivy.list
 apt-get update
-apt-get install trivy -y
+apt-get install trivy
